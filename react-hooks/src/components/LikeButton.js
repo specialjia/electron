c9ca40f62524c63bd88283932f2105c08a9cefc4
贾文强 <jiawenqiang@qishow.live>
2025-05-13 11:16:43 +0800

    useEffect

--- react-hooks/src/components/LikeButton.js
+++ react-hooks/src/components/LikeButton.js
@@ -1,16 +1,19 @@
-import React,{useState} from "react";
+import React,{useState, useEffect} from "react";
 
 const LikeButton = () => {
 
     const [like, setLike] = useState(0)
-    const [obj, setObj] = useState({like:0, on:true})
+    const [on, setOn] = useState(true)
+    useEffect(()=>{
+        document.title = `You liked ${like} times`
+    })
     return (
         <>
-        <button onClick={()=>{setObj({like:obj.like+1, on:obj.on})}}>
-            {obj.like}  👍
+        <button onClick={()=>{setLike(like+1)}}>
+            {like}  👍
         </button>
-        <button onClick={()=>{setObj({on:!obj.on, like:obj.like})}}>
-            {obj.on ? "ON" : "OFF"}
+        <button onClick={()=>{setOn(!on)}}>
+            {on ? "ON" : "OFF"}
         </button>
 
         </>
