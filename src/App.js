98584d72f0c4591b7dc3d7c3bc48608d02bf7f4b
贾文强 <jiawenqiang@qishow.live>
2025-05-22 10:39:39 +0800

    使用react开发electron项目

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import LikeButton from './components/LikeButton';
-import MouseTracker from './components/MouseTracker';
-import DogShow from './components/DogShow';
-import useMousePosition from './hooks/useMousePosition';
 
 function App() {
-  const position = useMousePosition()
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>{position.x}</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -23,9 +17,6 @@ function App() {
         >
           Learn React
         </a>
-        <LikeButton />
-        <MouseTracker />
-        <DogShow />
       </header>
     </div>
   );
