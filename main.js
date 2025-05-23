f9f3e5e2a47878d55dcac285f16aa86bb7809911
贾文强 <jiawenqiang@qishow.live>
2025-05-23 10:40:51 +0800

    成功生成安装包

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-const {app, BrowserWindow, ipcMain} = require('electron');
-const isDev = require('electron-is-dev');
+import path from 'path';
+import { app, BrowserWindow } from 'electron';
+import isDev from 'electron-is-dev';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-let mainWindow;
+// 当前模块的绝对文件路径
+const __filename = fileURLToPath(import.meta.url);
+// 当前模块所在目录
+const __dirname = dirname(__filename);
 
+console.log(__dirname);
+
+// let mainWindow;
+// console.log('App is run');
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({
+    console.log('App is ready');
+   const mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
@@ -13,9 +24,15 @@ app.on('ready', () => {
             enableRemoteModule: true
         }
     });
-
-    const urlLocation = isDev ? 'http://localhost:3000' :  'dummyurl';
-
-    mainWindow.loadURL(urlLocation);
+    mainWindow.webContents.openDevTools();
+    const urlLocation = isDev
+    ? 'http://localhost:3000'
+    : path.join(__dirname, './build/index.html');
+    console.log('urlLocation', path.join(__dirname, './build/index.html'));
+    if (isDev) {
+        mainWindow.loadURL(urlLocation);
+    } else {
+        mainWindow.loadFile(urlLocation);  // 注意，这里是 file path 不是 file:// URL
+    }
 
 });
